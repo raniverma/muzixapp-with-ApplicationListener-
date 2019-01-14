1e088a74fad5dd61e1a70027d9ddd068ff4beabd
pratima.verma <pratima.verma@boeing.com>
2019-01-15 00:18:30 +0530

    final commit

--- src/main/java/com/stackroute/MusicRest.java
+++ src/main/java/com/stackroute/MusicRest.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 @SpringBootApplication
-@Profile("dev")
 public class MusicRest implements ApplicationListener<ContextRefreshedEvent>{
 
 	public static void main(String[] args) {
