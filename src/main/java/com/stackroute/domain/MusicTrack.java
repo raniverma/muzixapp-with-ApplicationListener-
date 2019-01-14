24eea321c3149b653024a6ac3508df03e54f60c3
pratima.verma <pratima.verma@boeing.com>
2019-01-15 00:08:17 +0530

    final commit

--- src/main/java/com/stackroute/domain/MusicTrack.java
+++ src/main/java/com/stackroute/domain/MusicTrack.java
@@ -1,11 +1,16 @@
 package com.stackroute.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class MusicTrack {
 
     @Id
@@ -13,29 +18,5 @@ public class MusicTrack {
     private int trackId;
     private String trackName;
     private String  commentsOfTrack;
-
-    public int getTrackId() {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId) {
-        this.trackId = trackId;
-    }
-
-    public String getTrackName() {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getCommentsOfTrack() {
-        return commentsOfTrack;
-    }
-
-    public void setCommentsOfTrack(String commentsOfTrack) {
-        this.commentsOfTrack = commentsOfTrack;
-    }
 
 }
